f33e813655e7b753d08b7db3c48b7f309c5d3fbc
Nadahar <Nadahar@users.noreply.github.com>
2018-01-24 11:51:14 +0100

    - Created FileNamePrettifier that is similar to FileUtil.getFileNamePrettified() but keeps the parsed details for later use and doesn't use OpenSubtitles - Created VideoClassification for classifying a video as movie, series, TV program etc

--- src/main/java/net/pms/util/FileUtil.java
+++ src/main/java/net/pms/util/FileUtil.java
@@ -490,7 +490,7 @@ public class FileUtil {
 	 * Capitalize the first letter of each word if the string contains no
 	 * capital letters.
 	 */
-	private static String convertFormattedNameToTitleCaseParts(String formattedName) {
+	private static String convertFormattedNameToTitleCaseParts(String formattedName) { //TODO: (Nad) Remove
 		if (formattedName.equals(formattedName.toLowerCase())) {
 			StringBuilder formattedNameBuilder = new StringBuilder();
 			for (String part : formattedName.split(" - ")) {
@@ -508,7 +508,7 @@ public class FileUtil {
 	 * Capitalize the first letter of each word if the string contains no
 	 * capital letters.
 	 */
-	private static String convertFormattedNameToTitleCase(String formattedName) {
+	private static String convertFormattedNameToTitleCase(String formattedName) { //TODO: (Nad) Remove
 		if (formattedName.equals(formattedName.toLowerCase())) {
 			formattedName = convertLowerCaseStringToTitleCase(formattedName);
 		}
@@ -942,7 +942,7 @@ public class FileUtil {
 	 *
 	 * @return the converted string
 	 */
-	public static String convertLowerCaseStringToTitleCase(String value) {
+	public static String convertLowerCaseStringToTitleCase(String value) { //TODO: (Nad) Remove
 		if (isBlank(value)) {
 			return value;
 		}
