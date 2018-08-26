valib/UniversalMediaServer

Changed the DLNAMediaSubtitle.id() logic so it's only used for internal/embedded subtitles. External subtitles are recognized using isExternal() instead of id > 100

src/main/java/net/pms/dlna/DLNAResource.java
@@ -1661,7 +1661,7 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 
 				if (
 					media_subtitle != null &&
-					media_subtitle.getId() != -1 &&
+					media_subtitle.getId() != DLNAMediaLang.DUMMY_ID &&
 					configuration.isShowSubsInfo()
 				) {
 					if (nameSuffixBuilder.length() > 0) {
@@ -3527,7 +3527,9 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 	 * @return The resulting {@link BufferedImageFilterChain} or {@code null}.
 	 */
 	public BufferedImageFilterChain addSubtitlesFlagFilter(BufferedImageFilterChain filterChain) {
-		String subsLanguageCode = media_subtitle != null && media_subtitle.getId() != -1 ? media_subtitle.getLang() : null;
+		String subsLanguageCode = media_subtitle != null && media_subtitle.getId() != DLNAMediaLang.DUMMY_ID ?
+			media_subtitle.getLang() :
+			null;
 
 		if (isNotBlank(subsLanguageCode)) {
 			if (filterChain == null) {

onon765trb/DigitalMediaServer

src/main/java/net/pms/dlna/DLNAResource.java
@@ -1634,7 +1634,7 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 
 				if (
 					media_subtitle != null &&
-					media_subtitle.getId() != -1 &&
+					media_subtitle.getId() != DLNAMediaLang.DUMMY_ID &&
 					!configuration.hideSubsInfo()
 				) {
 					if (nameSuffixBuilder.length() > 0) {
@@ -3472,7 +3472,7 @@ public abstract class DLNAResource extends HTTPResource implements Cloneable, Ru
 	 */
 	@Nullable
 	public BufferedImageFilterChain addSubtitlesFlagFilter(@Nullable BufferedImageFilterChain filterChain) {
-		ISO639 subtitlesLanguage = media_subtitle != null && media_subtitle.getId() != -1 ?
+		ISO639 subtitlesLanguage = media_subtitle != null && media_subtitle.getId() != DLNAMediaLang.DUMMY_ID ?
 			media_subtitle.getLang() :
 			null;
 